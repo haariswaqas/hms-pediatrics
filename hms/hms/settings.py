3e23b26929a42440005a821126dc381719c36224
haariswaqas <haarisali9@gmail.com>
2025-09-24 12:22:07 +0000

    changed settings.py to print emails to the console

--- hms/hms/settings.py
+++ hms/hms/settings.py
@@ -269,7 +269,7 @@ CORS_ALLOW_HEADERS = [
 # ----------------------------
 # Email
 # ----------------------------
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
